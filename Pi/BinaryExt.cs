a875539c775029b572e09b2e392af8b19100c3b2
piruzhaolu <liyuluan@foxmail.com>
2019-04-10 21:37:04 +0800

    工具代码

---
+++ Pi/BinaryExt.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Mathematics;
+
+namespace Pi
+{
+    public static class BinaryWriterExt
+    {
+        public static void Write(this BinaryWriter bw, int3 value)
+        {
+            bw.Write(value.x);
+            bw.Write(value.y);
+            bw.Write(value.z);
+        }
+
+        public static void Write(this BinaryWriter bw, float3 value)
+        {
+            bw.Write(value.x);
+            bw.Write(value.y);
+            bw.Write(value.z);
+        }
+
+
+        public static void Write(this BinaryWriter bw, float4 value)
+        {
+            bw.Write(value.x);
+            bw.Write(value.y);
+            bw.Write(value.z);
+            bw.Write(value.w);
+        }
+    }
+
+    public static class BinaryReaderExt
+    {
+        public static int3 ReadInt3(this BinaryReader br)
+        {
+            return new int3(br.ReadInt32(), br.ReadInt32(), br.ReadInt32());
+        }
+
+        public static float3 ReadFloat3(this BinaryReader br)
+        {
+            return new float3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
+        }
+
+        public static float4 ReadFloat4(this BinaryReader br)
+        {
+            return new float4(br.ReadSingle(), br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
+        }
+
+    }
+}
---
+++ Pi/StringHash.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Pi
+{
+    public static class StringHash
+    {
+        private static Dictionary<int, string> hashDic = new Dictionary<int, string>();
+
+        /// <summary>
+        /// HashKeyToString 仅在编辑器中可用
+        /// </summary>
+        public static string ToString(int key)
+        {
+            string v = string.Empty;
+#if UNITY_EDITOR
+            hashDic.TryGetValue(key, out v);
+#endif
+            return v;
+        }
+
+
+        /// <summary>
+        /// 字符串hash函数
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static int ToHash(string name)
+        {
+            ////==========BKDRHash============
+
+            int seed = 131;
+            int hash = 0;
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                var a = name[i];
+                hash = hash * seed + (int)a;
+            }
+            int returnHash = hash & 0x7FFFFFFF;
+#if UNITY_EDITOR
+            string mName;
+
+            if (hashDic.TryGetValue(returnHash, out mName))
+            {
+                if (mName != name)
+                {
+                    Debug.LogError(name + "-HASH 冲突-" + mName);
+                }
+            }
+            hashDic[returnHash] = name;
+#endif
+            return (hash & 0x7FFFFFFF);
+        }
+
+
+        public static long ToHash64(string name)
+        {
+            ////==========BKDRHash============
+
+            int seed = 131;
+            long hash = 0;
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                var a = name[i];
+                hash = hash * seed + a;
+            }
+            long returnHash = hash & long.MaxValue;
+
+            return returnHash;
+        }
+    }
+}
