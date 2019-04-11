893dec86e48b9e7678f0337f71e772f9a3cafb6f
piruzhaolu <liyuluan@foxmail.com>
2019-04-11 22:09:01 +0800

    scriptable utils

---
+++ Pi/ScriptableObjectUtils.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Pi
+{
+    public class ScriptableObjectUtils
+    {
+        public static ScriptableObject CreatAsset(string name, Object tar)
+        {
+            ScriptableObject v = null;
+            try
+            {
+                v = ScriptableObject.CreateInstance(name);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+            v.name = name;
+            AssetDatabase.AddObjectToAsset(v, tar);
+            AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(tar));
+            return v;
+        }
+    }
+}
